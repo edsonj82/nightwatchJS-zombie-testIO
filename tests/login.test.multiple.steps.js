6d2db68c52b3a3350088ad7c8d64a7157b5969a1
Edson <edsonj82@outlook.com>
2020-03-01 14:46:03 -0300

    Multiple Steps like Cucumber

---
+++ tests/login.test.multiple.steps.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'dado que eu acesso a página de login': function (browser) {
+        browser
+            .resizeWindow(1920, 1000)
+            .url('http://zombie-api:5000/login')
+            .waitForElementVisible('.card-login', 3000)
+    },
+    'quando faço login com sucesso':function name(browser) {
+        browser
+        .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
+        .setValue('input[name=password]','pwd123')
+        .click('.login-button')
+    },
+    'então devo ver o meu nome na área logada': function name(browser) {
+        var userInfo = '.user .info span';
+        browser
+        .waitForElementVisible(userInfo, 3000)
+        .assert.containsText(userInfo, 'Quilombo')
+        .end();
+    }
+}
