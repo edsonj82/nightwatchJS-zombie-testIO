ec448021a2e08b83568b81150c3cd5d679399cca
Edson <edsonj82@outlook.com>
2020-03-01 17:24:10 -0300

    Alternative flows with steps

--- tests/login/login_sem_sucesso.test.js
+++ tests/login/login_sem_sucesso.test.js
@@ -1,6 +1,6 @@
 module.exports = {
     'senha incorreta': (browser) => {
-        let alert = '.alert';
+        let alert = '.alert-danger';
         browser
             .url('http://zombie-api:5000/login')
             .waitForElementVisible('.card-login', 3000)
@@ -8,7 +8,40 @@ module.exports = {
             .setValue('input[name=password]','123abc')
             .click('.login-button')
             .waitForElementVisible(alert,3000)
-            .assert.containsText(alert, 'Usuário e/ou senha inválidos')
-            .end();
+            .assert.containsText(alert, 'Usuário e/ou senha inválidos')            
+    },
+    'não cadastrado':(browser) =>{
+        let alert = '.alert-danger';
+        browser
+            .url('http://zombie-api:5000/login')
+            .waitForElementVisible('.card-login', 3000)
+            .setValue('input[name=email]', '404@yahoo.com')
+            .setValue('input[name=password]','123abc')
+            .click('.login-button')
+            .waitForElementVisible(alert,3000)
+            .assert.containsText(alert, 'Usuário e/ou senha inválidos')            
+    },
+    'email não informado':(browser) =>{
+        let alert = '.alert-info';
+        browser
+        .url('http://zombie-api:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+        .setValue('input[name=email]', '')
+        .setValue('input[name=password]','pwd123')
+        .click('.login-button')
+        .waitForElementVisible(alert,3000)
+        .assert.containsText(alert, 'Opps. Cadê o email?')      
+    },
+    'senha não informada':(browser) =>{
+        let alert = '.alert-info';
+        browser
+        .url('http://zombie-api:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+        .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
+        .setValue('input[name=password]','')
+        .click('.login-button')
+        .waitForElementVisible(alert,3000)
+        .assert.containsText(alert, 'Opps. Cadê a senha?')
+        .end();
     }
 }
