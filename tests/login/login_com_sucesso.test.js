8a4822379a383a4ccec4ffaa8343a99265e1f8d8
Edson <edsonj82@outlook.com>
2020-03-05 23:08:32 -0300

    Encapsulate Asserts

--- tests/login/login_com_sucesso.test.js
+++ tests/login/login_com_sucesso.test.js
@@ -6,17 +6,16 @@ module.exports = {
         let login = browser.page.login()
         let sidebar = browser.page.sidebar()
 
-        browser.resizeWindow(1920,1000);
+        // browser.resizeWindow(1920,1000);
 
-        login.navigate()
-            .with('zumbi@dospalmares.com.br','pwd123')
+        login.with('zumbi@dospalmares.com.br','pwd123')
             // .waitForElementVisible('@form', 3000)//.url('http://zombie-api:5000/login')
             // .setValue('@emailInput', 'zumbi@dospalmares.com.br')
             // .setValue('@passInput','pwd123')
             // .click('.login-button')
 
-        sidebar    
-            .waitForElementVisible('@userInfo', 3000)
-            .assert.containsText('@userInfo', 'Quilombo')
+        sidebar.expectLoggedUser('Quilombo')    
+            // .waitForElementVisible('@userInfo', 3000)
+            // .assert.containsText('@userInfo', 'Quilombo')
     }
 }
