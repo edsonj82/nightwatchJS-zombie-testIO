efc7fd58b144174b11f1f0e4d048a2881e21b61a
Edson <edsonj82@outlook.com>
2020-03-21 19:41:02 -0300

    Fix 404.test.js & (wip)busca.test.js

--- tests/busca/404.test.js
+++ tests/busca/404.test.js
@@ -17,8 +17,9 @@ module.exports = {
 
     'então devo ver uma mensagem de alerta': function (browser) {
         let movie = browser.page.movie()
-        browser
+        movie
             .waitForElementVisible('@alertDanger',10000)
+        movie    
             .assert.containsText('@alertDanger','Puxa! não encontramos nada aqui :(')
     }
 }
